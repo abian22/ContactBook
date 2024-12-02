ca800af25d8440afa6e09992e0f2dbe0d68f1c99
Abián <abian.camejo@gmail.com>
2024-12-02 18:28:13 +0000

    UserController file added and getUsers method done

---
+++ ContactBook/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using DB;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactBook.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private ContactContext _context;
+
+        public UserController(ContactContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> GetUsers()
+        {
+            var users = _context.Users.ToList();
+            return Ok(users);
+        }
+    }
+}
